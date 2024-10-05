bab0efd3b0b80818da2f5302c0d9a7ce0f7f5cf2
Yam Zebrak <yamzebrak@gmail.com>
2024-10-05 22:43:48 +0300

    Initial Redesign

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
+import TestHomePage from './pages/testhomepage';
 import HomePage from './pages/homepage';
 import ShopPage from './pages/shoppage';
 import Reviews from './pages/reviews';
@@ -8,9 +9,9 @@ import Reviews from './pages/reviews';
 function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<TestHomePage />} />
         <Route path="/shop" element={<ShopPage />} />
         <Route path="/reviews" element={<Reviews />} />
       </Routes>
