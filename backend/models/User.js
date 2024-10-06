7f7fe165855333525c4432488a6ffb026d1c8e02
HavitaBaCyber <bob.bestofbuild@gmail.com>
2024-10-06 22:29:45 +0300

    changed mongo user schema to have a role for admin check and added admincheck function

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,8 @@ const UserSchema = new mongoose.Schema({
   googleId: { type: String, unique: true, sparse: true },
   email: { type: String, unique: true, sparse: true },
   password: { type: String },
-  name: { type: String, required: true }
+  name: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' }
 });
 
 module.exports = mongoose.model('User', UserSchema);
