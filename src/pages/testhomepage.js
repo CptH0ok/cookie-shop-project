851ba83044f5a0b736d9bd588279c692dcf10f85
Yam Zebrak <yamzebrak@gmail.com>
2024-10-12 13:22:03 +0300

    Login button

--- src/pages/testhomepage.js
+++ src/pages/testhomepage.js
@@ -19,7 +19,7 @@ const TestHomePage = () => {
       <div className="bg-unsplash-[1Gv_4RcljOE/lg] z-10 relative">
       <div className="absolute flex sticky top-0 backdrop-blur-md backdrop-contrast-75 pt-3 pb-2 z-20 w-full drop-shadow-2xl">
       <div className="flex pl-4">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" class="size-12 stroke-white hover:stroke-yellow-500 duration-500">
+          <svg class="size-12 stroke-white/30 hover:stroke-white duration-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
         </div>
@@ -27,6 +27,15 @@ const TestHomePage = () => {
           <img src="https://img.icons8.com/fluency/48/cookies.png" className="ml-5 w-14 h-14 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"></img>
           <p className="pt-2 pl-1 text-4xl text-white font-serif font-bold">Cookie Land</p>
         </div>
+        <button href="#" className="mr-4 mt-2 relative flex z-10 h-10 rounded-md ring-1 text-white/30 stroke-white/30 ring-white/30 duration-500 hover:ring-white hover:stroke-white hover:text-white hover:backdrop-contrast-75 px-2.5 py-2.5 drop-shadow-md duration-500">
+          <a className="text-sm font-sans font-semibold">Sign Up</a>
+        </button>
+        <button href="#" className="mr-4 mt-2 relative flex z-10 h-10 rounded-md ring-1 text-white/30 stroke-white/30 ring-white/30 duration-500 hover:ring-white hover:stroke-white hover:text-white hover:backdrop-contrast-75 px-2.5 py-2.5 drop-shadow-md duration-500">
+          <svg class="pb-1 size-6"xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+          </svg>
+          <a className="text-sm font-sans font-semibold">Login</a>
+        </button>
       </div>
         <div class="absolute inset-0 bg-black bg-opacity-75 z-0"></div>
         <div
