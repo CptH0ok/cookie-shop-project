5f4ddbfb26246aad66a52dc473934c3a25b7daf8
Yam Zebrak <yamzebrak@gmail.com>
2024-10-22 22:41:56 +0300

    Navbar start

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle, TransitionChild } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(true)
 
   const handleLogout = () => {
     localStorage.removeItem("token"); // Remove token on logout
@@ -11,7 +15,7 @@ const Navbar = () => {
 
   return (
     <nav className="flex absolute backdrop-blur-md backdrop-contrast-75 pt-3 pb-2 z-20 w-full drop-shadow-2xl">
-      <div className="flex pl-4">
+      <button className="flex pl-4" onClick={() => setOpen(true)}>
         <svg
           class="size-12 stroke-white/30 hover:stroke-white duration-500"
           xmlns="http://www.w3.org/2000/svg"
@@ -25,7 +29,7 @@ const Navbar = () => {
             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
           />
         </svg>
-      </div>
+      </button>
       <div className="flex justify-center w-full">
         <img
           src="https://img.icons8.com/fluency/48/cookies.png"
@@ -75,6 +79,45 @@ const Navbar = () => {
         </svg>
         <a className="text-sm font-sans font-semibold">Login</a>
       </Link>
+
+
+      <Dialog open={open} onClose={setOpen} className="relative z-20">
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity duration-500 ease-in-out data-[closed]:opacity-0"
+      />
+
+      <div className="fixed inset-0 overflow-hidden">
+        <div className="absolute inset-0 overflow-hidden">
+          <div className="pointer-events-none fixed inset-y-0 left-0 flex max-w-full ">
+            <DialogPanel
+              transition
+              className="pointer-events-auto relative w-screen max-w-md transform transition duration-500 ease-in-out data-[closed]:translate-x-full sm:duration-700"
+            >
+              <TransitionChild>
+                <div className="absolute left-0 top-0 -ml-8 flex pr-2 pt-4 duration-500 ease-in-out data-[closed]:opacity-0 sm:-ml-10 sm:pr-4">
+                  <button
+                    type="button"
+                    onClick={() => setOpen(false)}
+                    className="relative rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                  >
+                    <span className="absolute -inset-2.5" />
+                    <span className="sr-only">Close panel</span>
+                    <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+                  </button>
+                </div>
+              </TransitionChild>
+              <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                <div className="px-4 sm:px-6">
+                  <DialogTitle className="text-base font-semibold leading-6 text-gray-900">Panel title</DialogTitle>
+                </div>
+                <div className="relative mt-6 flex-1 px-4 sm:px-6">{/* Your content */}</div>
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </div>
+    </Dialog>
     </nav>
   );
 };
