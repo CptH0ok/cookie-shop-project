0b004ee29678f84d3d6f06b1f6bb4f36b8c5debd
Yam Zebrak <yamzebrak@gmail.com>
2024-10-23 21:51:39 +0300

    Split App.js
    Split App.js into AppContent.js with a list of pages that include the navbar and the footer (so that the 404 page will not include it)

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Footer from './components/footer';
-import HomePage from './pages/homepage';
-import ShopPage from './pages/shoppage';
-import ErrorPage from './pages/error';
-import Reviews from './pages/reviews';
-import Login from './pages/login';  // Login component
-import Signup from './pages/signup';  // Signup component
-import Maps from './pages/maps'
-import ProtectedRoute from './components/ProtectedRoute';  // The protected route component
-import LoginSuccess from './components/LoginSuccess';
-import Admin from './pages/admin';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppContent from './AppContent';
 
 function App() {
   return (
     <Router>
-      <div className='w-full sticky top-0 z-20'>
-        <Navbar />
-      </div>
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-
-        {/* Handle Google Login Success */}
-        <Route path="/login/success" element={<LoginSuccess />} />
-
-        {/* Protected Routes */}
-        <Route path="/shop" element={<ProtectedRoute element={<ShopPage />} />} />
-        <Route path="/maps" element={<ProtectedRoute element={<Maps />} />} />
-        <Route path="/reviews" element={<ProtectedRoute element={<Reviews />} />} />
-        <Route path="/admin" element={<ProtectedRoute element={<Admin />} />} />
-
-        {/* Wildcard route: Let backend handle unhandled paths*/}
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-      <Footer />
+      <AppContent />
     </Router>
   );
 }
