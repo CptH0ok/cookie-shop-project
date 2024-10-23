eb279129aa320d8cd4db31ed6cdf24b21674eb00
Yam Zebrak <yamzebrak@gmail.com>
2024-10-23 22:59:04 +0300

    404 page done

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,9 +1,25 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import error from "../assets/error.png"
+import bg from "../assets/errbg.png"
 
 const ErrorPage = () => {
   return (
-    <div></div>
+    <div className="relative pt-2 left-0 min-h-screen" style={{
+      backgroundImage: `url(${bg})`, // Use the imported image here
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center'
+    }}>
+      <div className="flex mt-12 justify-center items-center h-20">
+        <img src={error} alt="Error" className="max-w-xs" />
+      </div>
+      <h1 className="relative mt-6 text-center text-balance text-white text-4xl font-bold font-serif tracking-tight drop-shadow-2xl sm:text-6xl">
+                  Oops! The Oven’s Empty!
+      </h1>
+      <p className="relative mt-1 text-lg text-center leading-8 text-white drop-shadow-2xl font-serif">
+            Looks like this page doesn’t exist anymore. Maybe it got eaten? Why not check out our latest cookie flavors instead!
+      </p>
+    </div>
   );
 };
 
