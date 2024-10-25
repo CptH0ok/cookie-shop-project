08274991dce249d4088d088f588fd9b4417c3b84
Ethan Nevelev <ethannevelev@Ethans-MacBook-Pro.local>
2024-10-24 22:35:55 -0400

    code cleanup and file naming convention

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
-import GoogleSignIn from '../components/GoogleSignIn';
+import GoogleSignIn from '../components/googlesignin';
 
 const Login = () => {
   const [error, setError] = useState('');
