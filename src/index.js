ab790ee70ef05092be636645c63fadfe55326c15
Ethan Nevelev <ethannevelev@Ethans-MacBook-Pro.local>
2024-10-25 01:38:41 -0400

    now checks for admin role in backend and works

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './tailwind.css';
 import './index.css';
-import App from './app';
-import reportWebVitals from './reportwebvtals';
+import App from './App';
+import reportWebVitals from './reportwebvitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
