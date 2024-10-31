fb9524d3634874b0eae2fd2dd316dcef0ff8c6d6
shellyoron <212sheli11@gmail.com>
2024-10-31 16:10:09 +0200

    about us font change
    changed the fonts and added to styles

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -23,8 +23,8 @@ const About1 = () => {
       <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
           <div className="flex flex-col lg:flex-row justify-between gap-8">
               <div className="w-full lg:w-5/12 flex flex-col justify-center">
-                  <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">About Us</h1>
-                  <p className="font-normal text-base leading-6 text-gray-600 "> At Cookie Land, we believe in using only the finest ingredients, supporting local suppliers, and creating a warm, welcoming environment for our customers. Our commitment to quality and community is baked into every cookie we make.</p>
+                  <h1 className="title_black">About Us</h1>
+                  <p className="paragraph_small "> At Cookie Land, we believe in using only the finest ingredients, supporting local suppliers, and creating a warm, welcoming environment for our customers. Our commitment to quality and community is baked into every cookie we make.</p>
               </div>
               <div className="w-full lg:w-8/12 ">
                   <img className="w-full h-full" src="https://images.unsplash.com/photo-1464979681340-bdd28a61699e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A shop" />
@@ -33,30 +33,32 @@ const About1 = () => {
 
           <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
               <div className="w-full lg:w-5/12 flex flex-col justify-center">
-                  <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">Our Story</h1>
-                  <p className="font-normal text-base leading-6 text-gray-600 ">Cookie Land was born from a simple love for homemade cookies. What started as weekend baking sessions for friends and family in 2023 quickly grew into the beloved cookie shop you know today. Our founder, Jane Doe, turned her passion into a mission: to spread happiness one cookie at a time.</p>
+                  <h1 className="title_black">Our Story</h1>
+                  <p className="paragraph_small">
+                    Cookie Land was born from a simple love for homemade cookies. What started as weekend baking sessions for friends and family in 2023 quickly grew into the beloved cookie shop you know today. Our founder, Jane Doe, turned her passion into a mission: to spread happiness one cookie at a time.
+                    </p>
               </div>
               <div className="w-full lg:w-8/12 lg:pt-8">
                   <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
                       <div className="p-4 pb-6 flex justify-center flex-col items-center">
                           <img className="md:block hidden" src="https://i.ibb.co/FYTKDG6/Rectangle-118-2.png" alt="Alexa featured Img" />
                           <img className="md:hidden block" src="https://i.ibb.co/zHjXqg4/Rectangle-118.png" alt="Alexa featured Img" />
-                          <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Yam</p>
+                          <p className="font-serif text-xl leading-5 text-gray-800 mt-4">Yam</p>
                       </div>
                       <div className="p-4 pb-6 flex justify-center flex-col items-center">
                           <img className="md:block hidden" src="https://i.ibb.co/fGmxhVy/Rectangle-119.png" alt="Olivia featured Img" />
                           <img className="md:hidden block" src="https://i.ibb.co/NrWKJ1M/Rectangle-119.png" alt="Olivia featured Img" />
-                          <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Adi</p>
+                          <p className="font-serif text-xl leading-5 text-gray-800 mt-4">Adi</p>
                       </div>
                       <div className="p-4 pb-6 flex justify-center flex-col items-center">
                           <img className="md:block hidden" src="https://i.ibb.co/Pc6XVVC/Rectangle-120.png" alt="Liam featued Img" />
                           <img className="md:hidden block" src="https://i.ibb.co/C5MMBcs/Rectangle-120.png" alt="Liam featued Img" />
-                          <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Eitan</p>
+                          <p className="font-serif text-xl leading-5 text-gray-800 mt-4">Eitan</p>
                       </div>
                       <div className="p-4 pb-6 flex justify-center flex-col items-center">
                           <img className="md:block hidden" src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png" alt="Elijah featured img" />
                           <img className="md:hidden block" src="https://i.ibb.co/ThZBWxH/Rectangle-121.png" alt="Elijah featured img" />
-                          <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Shelly</p>
+                          <p className="font-serif text-xl leading-5 text-gray-800 mt-4">Shelly</p>
                       </div>
                   </div>
               </div>
