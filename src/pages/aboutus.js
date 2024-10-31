e3378dabd57ae051b4cecfae46e11c6f9a9ca86a
shellyoron <212sheli11@gmail.com>
2024-10-31 15:52:18 +0200

    about us page
    final look of the page

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -6,8 +6,7 @@ import '/Users/shellyoron/cookie-shop-project/src/styles/styles.css';
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const AboutUsPage = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+const About1 = () => {
   return (
     <div className="relative flex flex-col z-10 min-h-screen h-auto">
       <div className="relative bg-unsplash-[1Gv_4RcljOE/lg] bg-no-repeat bg-cover z-10">
@@ -21,37 +20,51 @@ const AboutUsPage = () => {
           </div>
         </div>
       </div>
-      <body className="relative bg-black h-auto">
-        <div className="flex h-auto">
-          <h1 className="bg-black w-1/2 h-auto pt-12 text-center text-balance text-white text-6xl font-serif font-bold">
-            Our Story
-            <p className="paragraph">
-              Cookie Land was born from a simple love for homemade cookies. What started as weekend baking sessions for friends and family in 2010 quickly grew into the beloved cookie shop you know today. Our founder, Jane Doe, turned her passion into a mission: to spread happiness one cookie at a time.
-            </p>
-          </h1>
-          <div className="relative right-0 bg-unsplash-[4ZgmCmPXMmI] bg-cover bg-center bg-no-repeat w-1/2 h-auto"></div>
-        </div>
-        <div className="flex h-auto">
-        <div className="relative left-0 bg-unsplash-[F2eHfMwIOxA] bg-cover bg-bottom bg-no-repeat w-1/2 h-auto"></div>
-          <h1 className="bg-black w-1/2 h-auto pt-12 text-center text-balance text-white text-6xl font-serif font-bold">
-            Our Values
-            <p className="paragraph">
-              At Cookie Land, we believe in using only the finest ingredients, supporting local suppliers, and creating a warm, welcoming environment for our customers. Our commitment to quality and community is baked into every cookie we make.
-            </p>
-          </h1>
-        </div>
-        <div className="flex h-auto">
-          <h1 className="bg-black w-1/2 h-auto pt-12 text-center text-balance text-white text-6xl font-serif font-bold">
-          Meet Our Team
-            <p className="paragraph">
-              From our skilled bakers to our friendly staff, every member of the Cookie Land family plays a crucial role in bringing you the perfect cookie experience. We're united by our love for baking and our dedication to customer satisfaction.
-            </p>
-          </h1>
-          <div className="relative right-0 bg-unsplash-[Zyx1bK9mqmA] bg-cover bg-top bg-no-repeat w-1/2 h-auto"></div>
-        </div>
-      </body>
-    </div>
+      <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
+          <div className="flex flex-col lg:flex-row justify-between gap-8">
+              <div className="w-full lg:w-5/12 flex flex-col justify-center">
+                  <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">About Us</h1>
+                  <p className="font-normal text-base leading-6 text-gray-600 "> At Cookie Land, we believe in using only the finest ingredients, supporting local suppliers, and creating a warm, welcoming environment for our customers. Our commitment to quality and community is baked into every cookie we make.</p>
+              </div>
+              <div className="w-full lg:w-8/12 ">
+                  <img className="w-full h-full" src="https://images.unsplash.com/photo-1464979681340-bdd28a61699e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A shop" />
+              </div>
+          </div>
+
+          <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
+              <div className="w-full lg:w-5/12 flex flex-col justify-center">
+                  <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">Our Story</h1>
+                  <p className="font-normal text-base leading-6 text-gray-600 ">Cookie Land was born from a simple love for homemade cookies. What started as weekend baking sessions for friends and family in 2023 quickly grew into the beloved cookie shop you know today. Our founder, Jane Doe, turned her passion into a mission: to spread happiness one cookie at a time.</p>
+              </div>
+              <div className="w-full lg:w-8/12 lg:pt-8">
+                  <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
+                      <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                          <img className="md:block hidden" src="https://i.ibb.co/FYTKDG6/Rectangle-118-2.png" alt="Alexa featured Img" />
+                          <img className="md:hidden block" src="https://i.ibb.co/zHjXqg4/Rectangle-118.png" alt="Alexa featured Img" />
+                          <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Yam</p>
+                      </div>
+                      <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                          <img className="md:block hidden" src="https://i.ibb.co/fGmxhVy/Rectangle-119.png" alt="Olivia featured Img" />
+                          <img className="md:hidden block" src="https://i.ibb.co/NrWKJ1M/Rectangle-119.png" alt="Olivia featured Img" />
+                          <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Adi</p>
+                      </div>
+                      <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                          <img className="md:block hidden" src="https://i.ibb.co/Pc6XVVC/Rectangle-120.png" alt="Liam featued Img" />
+                          <img className="md:hidden block" src="https://i.ibb.co/C5MMBcs/Rectangle-120.png" alt="Liam featued Img" />
+                          <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Eitan</p>
+                      </div>
+                      <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                          <img className="md:block hidden" src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png" alt="Elijah featured img" />
+                          //*<img className="md:hidden block" src="https://i.ibb.co/ThZBWxH/Rectangle-121.png" alt="Elijah featured img" />
+                          <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Shelly</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      </div>
   );
 };
 
-export default AboutUsPage;
+
+export default About1;
