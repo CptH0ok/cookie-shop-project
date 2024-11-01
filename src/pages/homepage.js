5cbe4e731ea4521f8af3637ef40b18aaba956bc8
adiswork <124614709+adiswork@users.noreply.github.com>
2024-11-01 19:43:29 +0200

    ordernow button -> /shop and readmore -> /glutenfree

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -16,7 +16,7 @@ const HomePage = () => {
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="text-white relative rounded-full px-3 py-1 z-10 text-base font-serif leading-6 ring-1 ring-white/10 hover:ring-white/20">
               New Gluten Free Options!{" "}
-              <a href="#" className="font-semibold font-serif text-yellow-600">
+              <a href="/glutenfree" className="font-semibold font-serif text-yellow-600">
                 <span aria-hidden="true" className="absolute inset-0" />
                 Read more <span aria-hidden="true">&rarr;</span>
               </a>
@@ -34,7 +34,7 @@ const HomePage = () => {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="#"
+                href="/shop"
                 className="relative z-10 rounded-md bg-yellow-600 px-3.5 py-2.5 text-base font-serif duration-500 font-semibold text-white drop-shadow-md hover:bg-yellow-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
               >
                 Order Now
