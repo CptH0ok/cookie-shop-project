3a86a7f7adc5cb456ed4d2a094b6ebecc7662128
HavitaBaCyber <bob.bestofbuild@gmail.com>
2024-11-09 18:19:13 +0200

    made facebook api entiraly independant

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const FB = require('./facebookapi');
+const boom = require('@hapi/boom');
+const facebookApi = require('./facebookapi');
 const mongoose = require('mongoose');
 const usersApi = require('./usersapi');
 const securityApi = require('./security');
@@ -12,13 +13,13 @@ const branchesApi = require('./branchesapi');
 const convertCurrency = require('./currencyapi');
 const {authenticateJWT, checkAdmin} = require('./middlewares');
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersApi );
 app.use('/api/cookies', cookiesApi );
 app.use('/api/security', securityApi );
 app.use('/api/branches', branchesApi );
+app.use('/api/facebook', facebookApi );
 app.use('/api/currency', convertCurrency );
 
 // Serve static images from the "images" folder
@@ -28,55 +29,17 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 mongoose.connect(process.env.MONGO_URI, {dbName: 'cookie_DB'}
   ).then(() => console.log('MongoDB Connected'));
 
-
-// Routes
-app.get('/api/products', async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
-});
-
-app.get('/api/pagereviews', async (req, res, next) => {
-  try {
-    const reviews = await FB.getPageReviews();
-    res.json(reviews);
-  } catch (error) {
-    next(error);
-  }
-  
-});
-
-app.get('/api/getlastdataphoto', async (req, res, next) => {
-  try {
-    const lastPostId = await FB.getLastPost();
-    const photo = await FB.getPostPicture(lastPostId);
-    res.json(photo);
-  } catch (error) {
-    next(error);
-  }
-  
-});
-
-app.get('/api/getlastdatacomments', async (req, res, next) => {
-  try{
-    const lastPostId = await FB.getLastPost();
-    const comments = await FB.getPostComments(lastPostId);
-    res.json(comments);
-  } catch (error) {
-      next(error);
-  }
-});
-
 app.get('/api/admin', authenticateJWT, checkAdmin, (req, res) => {
   res.json('Welcome "' + req.user.name + '" to admin panel!');
 });
 
-// Error handling middleware
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
-    error: {
-      message: err.response.data.error.message || 'Internal Server Error',
-    },
-  });
+  if (!err.isBoom) {
+    next(err);
+    return;
+  }
+  const { output } = err;
+  res.status(output.statusCode).json({ error: output.payload });
 });
 
 // Start server
