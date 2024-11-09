6f9cc5ce01d676db3c615608d62134437ecc6be6
shellyoron <212sheli11@gmail.com>
2024-11-09 22:00:20 +0200

    ui change in cart

--- src/pages/cartpage.js
+++ src/pages/cartpage.js
@@ -281,7 +281,7 @@ useEffect(() => {
 
   return (
     <div className="h-screen bg-gray-100 pt-20">
-      <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
+      <h1 className="mb-10 text-center text-2xl font-bold"></h1>
       <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
         <div className="rounded-lg md:w-2/3">
           {loading ? (
